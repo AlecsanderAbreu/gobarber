69216cca4dfad4e886ae037bb548777f65aaaf54
Alecsander Abreu <alecsanderabreu@gmail.com>
2019-09-01 10:00:43 -0300

    Implementado listagem de providers

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,11 +1,18 @@
 import Sequelize, { Model } from 'sequelize';
+import { get } from 'https';
 
 class File extends Model {
   static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
-        path: Sequelize.STRING
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          }
+        }
       },
       {
         sequelize
